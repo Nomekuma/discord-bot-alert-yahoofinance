1b93c3605c61cb9996dab31fb8316ef189f38170
Nomekuma <t.gunness35@gmail.com>
2025-08-25 11:04:43 +0400

    feat: add test alert to indicate bot is running

--- discord/discordBot.js
+++ discord/discordBot.js
@@ -67,6 +67,11 @@ async function botTick(client) {
   const toNotify = [];
   for (const r of results) {
     if (!r) continue;
+    // this is a test alert to show bot is running
+    if (results.indexOf(r) === 0) {
+      toNotify.push({ symbol: "-----", alert: "-----" });
+      continue;
+    }
     const last = lastAlertBySymbol.get(r.symbol);
     if (r.alert !== "none" && r.alert !== last) {
       toNotify.push(r);
